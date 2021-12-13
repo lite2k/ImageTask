6e9f91745134f863e5e44c13de6ca43c41613211
Youssef El Sayed <ysayed38@gmail.com>
2021-12-14 00:34:48 +0200

    Finished Objects_with_hole function. Changed Variable names to distinguish types of images.

--- Main.m
+++ Main.m
@@ -1,23 +1,29 @@
 Image = imread('C:\Users\DELL\Desktop\image processing\Lab 7\Task\morph.png');
-Image = rgb2gray(Image);
-Image = imbinarize(Image);
+grey_image = rgb2gray(Image);
+bin_image = imbinarize(Image);
+logical_image = logical(Image);
 complement = ~Image
 
-Fraction = WhiteCount(Image);
+%%%%%%%%%%%%%%%%%%%%
+
+Fraction = WhiteCount(bin_image);
 Fraction
 
-%%%%%%%
-ObjectsCount = CountObjects(Image);
-ObjectsCount
 %%%%%%%%%%%%%
+
+ObjectsCount = CountObjects(bin_image);
+ObjectsCount
+
+%%%%%%%%%%%%%
+
 HolesCount = CountHoles(complement);
 HolesCount
+
 %%%%%%%%%%%%%%%
-Sqaurescount = CountSquares(Image);
-owh_count = Objects_With_holes(Image);
 
+owh_count = Objects_With_holes(logical_image);
+owh_count
 
+%%%%%%%%%%%%%%%
 
-
-
-
+Sqaurescount = CountSquares(bin_image);
--- Objects_With_holes.m
+++ Objects_With_holes.m
@@ -1,9 +1,10 @@
-function [ owh_count ] = Objects_With_holes( Image )
-se = strel('square',10);
-im = imopen(Image,se);
-figure,imshow(im);
-[ label, owh_count ] = bwlabel(im);
+function [owh_count] = Objects_With_holes(Image)
+    %se = strel('square',10);
+    %im = imopen(Image,se);
+    %figure,imshow(im);
+    %[ label, owh_count ] = bwlabel(im);
 
+    owh_im = regionprops(Image, 'EulerNumber');
+    owh_count = find([STATS.EulerNumber] <= 0);
 
 end
-
